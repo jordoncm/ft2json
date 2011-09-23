e562b29f15c9f489a67dda7efe1edd6c52f062b3
Jordon Mears <jordoncm@gmail.com>
2011-09-23 12:23:33 -0400

    Finished first pass.

--- handlers.py
+++ handlers.py
@@ -1,9 +1,9 @@
 import csv
+import httplib
 import json
 import StringIO
 import tornado.web
 import urllib
-import urllib2
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
@@ -30,69 +30,75 @@ class IndexHandler(tornado.web.RequestHandler):
         sql = self.get_argument('sql', '')
         
         if sql != '':
-            # try:
-            type = sql.strip().split(' ')[0].lower()
             try:
-                if getCommands.index(type) >= 0:
-                    method = 'get'
-            except:
-                pass
-            try:
-                if postCommands.index(type) >= 0:
-                    method = 'post'
-            except:
-                pass
-            
-            if type == 'select':
-                start = int(self.get_argument('start', 0))
-                limit = int(self.get_argument('limit', 100))
+                type = sql.strip().split(' ')[0].lower()
+                try:
+                    if getCommands.index(type) >= 0:
+                        method = 'get'
+                except:
+                    pass
+                try:
+                    if postCommands.index(type) >= 0:
+                        method = 'post'
+                except:
+                    pass
                 
-                sql += ' OFFSET ' + str(start) + ' LIMIT ' + str(limit)
+                if type == 'select':
+                    start = int(self.get_argument('start', 0))
+                    limit = int(self.get_argument('limit', 100))
+                    
+                    sql += ' OFFSET ' + str(start) + ' LIMIT ' + str(limit)
+                    
+                    output['start'] = start
+                    output['limit'] = limit
                 
-                output['start'] = start
-                output['limit'] = limit
-            try:
                 parameters = {
                     'sql' : sql
                 }
                 accessToken = self.get_argument('access_token', '')
                 if accessToken != '':
                     parameters['access_token'] = accessToken
+                
+                connection = httplib.HTTPSConnection('www.google.com')
                 if method == 'get':
-                    request = urllib2.Request(
-                        'https://www.google.com/fusiontables/api/query?' + urllib.urlencode(parameters)
+                    connection.request(
+                        'GET',
+                        '/fusiontables/api/query?' + urllib.urlencode(parameters)
                     )
                 elif method == 'post':
-                    request = urllib2.Request(
-                        'https://www.google.com/fusiontables/api/query',
+                    connection.request(
+                        'POST',
+                        '/fusiontables/api/query?' + urllib.urlencode(parameters),
                         urllib.urlencode(parameters),
                         {'Content-Type' : 'application/x-www-form-urlencoded'}
                     )
-                response = urllib2.urlopen(request)
+                response = connection.getresponse()
                 file = StringIO.StringIO(response.read())
-            except urllib2.HTTPError, e:
-                file = StringIO.StringIO(e.read())
-                self.write(file.getvalue())
-            
-            count = 0
-            keys = []
-            data = []
-            for row in csv.reader(file):
-                if len(keys) <= 0:
-                    keys = row
+                
+                if response.status == 200:
+                    count = 0
+                    keys = []
+                    data = []
+                    for row in csv.reader(file):
+                        if len(keys) <= 0:
+                            keys = row
+                        else:
+                            tmp = {}
+                            for i in range(len(row)):
+                                tmp[keys[i]] = row[i]
+                            count += 1
+                            data.append(tmp)
+                    
+                    output['count'] = count
+                    output['data'] = data
+                    
+                    success = True
                 else:
-                    tmp = {}
-                    for i in range(len(row)):
-                        tmp[keys[i]] = row[i]
-                    count += 1
-                    data.append(tmp)
-            
-            output['count'] = count
-            output['data'] = data
-            
-            success = True
-            # except:
-            #     success = False
+                    success = False
+                    output['error'] = [file.getvalue()]    
+            except:
+                success = False
+                output['error'] = ['Bad input parameters.']
         else:
             success = False
         
